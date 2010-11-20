d7bb9302843e81a2e29b06de579ec663c65a8b65
Pavel Kalian <pavel@kalian.cz>
2010-11-20 15:35:50 +0100

    Initial import

---
+++ LogBookExtractor/LogBookExtractorFrm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace LogBookExtractor
+{
+    partial class LogBookExtractorFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.tbInputLog = new System.Windows.Forms.TextBox();
+            this.btnBrowseLog = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBrowseGpx = new System.Windows.Forms.Button();
+            this.tbOutputGpx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(205, 100);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // tbInputLog
+            // 
+            this.tbInputLog.Location = new System.Drawing.Point(12, 25);
+            this.tbInputLog.Name = "tbInputLog";
+            this.tbInputLog.Size = new System.Drawing.Size(187, 20);
+            this.tbInputLog.TabIndex = 1;
+            // 
+            // btnBrowseLog
+            // 
+            this.btnBrowseLog.Location = new System.Drawing.Point(205, 25);
+            this.btnBrowseLog.Name = "btnBrowseLog";
+            this.btnBrowseLog.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseLog.TabIndex = 2;
+            this.btnBrowseLog.Text = "...";
+            this.btnBrowseLog.UseVisualStyleBackColor = true;
+            this.btnBrowseLog.Click += new System.EventHandler(this.btnBrowseLog_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "OpenCPN Logfile";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "GPX Output";
+            // 
+            // btnBrowseGpx
+            // 
+            this.btnBrowseGpx.Location = new System.Drawing.Point(205, 64);
+            this.btnBrowseGpx.Name = "btnBrowseGpx";
+            this.btnBrowseGpx.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseGpx.TabIndex = 5;
+            this.btnBrowseGpx.Text = "...";
+            this.btnBrowseGpx.UseVisualStyleBackColor = true;
+            this.btnBrowseGpx.Click += new System.EventHandler(this.btnBrowseGpx_Click);
+            // 
+            // tbOutputGpx
+            // 
+            this.tbOutputGpx.Location = new System.Drawing.Point(12, 64);
+            this.tbOutputGpx.Name = "tbOutputGpx";
+            this.tbOutputGpx.Size = new System.Drawing.Size(187, 20);
+            this.tbOutputGpx.TabIndex = 4;
+            // 
+            // LogBookExtractorFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 135);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowseGpx);
+            this.Controls.Add(this.tbOutputGpx);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowseLog);
+            this.Controls.Add(this.tbInputLog);
+            this.Controls.Add(this.btnGo);
+            this.Name = "LogBookExtractorFrm";
+            this.Text = "LogBookExtractor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox tbInputLog;
+        private System.Windows.Forms.Button btnBrowseLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBrowseGpx;
+        private System.Windows.Forms.TextBox tbOutputGpx;
+    }
+}
+
---
+++ LogBookExtractor/LogBookExtractorFrm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LogBookExtractor
+{
+    using System.Globalization;
+    using System.IO;
+    using System.Xml.Serialization;
+
+    public partial class LogBookExtractorFrm : Form
+    {
+        public LogBookExtractorFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseLog_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            DialogResult res = dlg.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                tbInputLog.Text = dlg.FileName;
+            }
+        }
+
+        private void btnBrowseGpx_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            DialogResult res = dlg.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                tbOutputGpx.Text = dlg.FileName;
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(tbInputLog.Text))
+            {
+                MessageBox.Show(
+                    "You have to select a log file first!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                StreamReader r = new StreamReader(tbInputLog.Text);
+                string s;
+                string ps = String.Empty;
+                DateTime date = new DateTime();
+                TimeSpan entrytime = new TimeSpan();
+                double latitude = 0;
+                double longitude = 0;
+                gpxType gpx = new gpxType();
+                gpx.creator = "OpenCPN LogBookExtractor";
+                gpx.version = "1.1";
+                List<trkType> trks = new List<trkType>();
+                trkType trk = new trkType();
+                trksegType seg = new trksegType();
+                List<wptType> trkpts = new List<wptType>();
+                int trknum = 0;
+                while ((s = r.ReadLine()) != null)
+                {
+                    while (s.Contains("  "))
+                    {
+                        s = s.Replace("  ", " ");
+                    }
+                    if (s.Contains("-------Starting opencpn-------"))
+                    {
+                        if (trknum > 0)
+                        {
+                            seg.trkpt = trkpts.ToArray();
+                            trk.trkseg[0] = seg;
+                            trks.Add(trk);
+                        }
+                        date = DateTime.Parse(ps.Split(' ')[1]);
+                        trk = new trkType
+                            {
+                                name = String.Format("Logbook {0}", date.ToShortDateString()),
+                                number = (trknum + 100).ToString(),
+                                trkseg = new trksegType[1]
+                            };
+                        seg = new trksegType();
+                        trkpts.Clear();
+                        trknum++;
+                    }
+                    if (s.Contains("LOGBOOK"))
+                    {
+                        string[] sa = s.Split(' ');
+                        if (sa[0].EndsWith(":"))
+                        {
+                            entrytime = TimeSpan.Parse(sa[0].Substring(0, sa[0].Length - 1));
+                        }
+                        else
+                        {
+                            entrytime = TimeSpan.Parse(sa[0]);
+                            if (sa[1] == "PM:")
+                            {
+                                entrytime.Add(new TimeSpan(12, 0, 0));
+                            }
+                        }
+                        int i = 0;
+                        foreach (var s1 in sa)
+                        {
+                            if (s1 == "Lat")
+                            {
+                                latitude = double.Parse(sa[i + 1], CultureInfo.InvariantCulture);        
+                            }
+                            if (s1 == "Lon")
+                            {
+                                longitude = double.Parse(sa[i + 1], CultureInfo.InvariantCulture);
+                            }
+                            i++;
+                        }
+                        trkpts.Add(new wptType() { lat = (decimal)latitude, lon = (decimal)longitude, time = date.Add(entrytime), timeSpecified = true });
+                    }
+                    ps = s;
+                }
+                gpx.trk = trks.ToArray();
+                XmlSerializer ser = new XmlSerializer(typeof(gpxType));
+                StreamWriter w = new StreamWriter(tbOutputGpx.Text);
+                ser.Serialize(w, gpx);
+                w.Close();
+            }
+        }
+    }
+}
